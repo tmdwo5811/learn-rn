2349ae2b23d803d5360df797585582ff3a4cd578
tmdwo5811 <tmdwo5811@gmail.com>
2026-07-01 13:34:00 +0900

    스타일 시트 적용(변수화)

--- threads/src/app/(tabs)/(home)/index.tsx
+++ threads/src/app/(tabs)/(home)/index.tsx
@@ -1,4 +1,4 @@
-import {Text, TouchableOpacity, View} from "react-native";
+import {Text, TouchableOpacity, View, StyleSheet} from "react-native";
 import {usePathname, useRouter} from "expo-router";
 // 주소는 변경되지만 화면은 공유해서 사용하는 영역
 export default function Index() {
@@ -6,37 +6,47 @@ export default function Index() {
     const pathName = usePathname();
     console.log("pathName", pathName);
     return (
-        <View style={{
-            flex: 1,
-            alignItems: "center",
-            justifyContent: "center",
-        }}>
-            <View>
-                <TouchableOpacity onPress={() => router.push(`/`)}>
-                    <Text style={{color: pathName ==="/" ?"red": "black"}}>For you</Text>
-                </TouchableOpacity>
-            </View>
-
-            <View>
-                <TouchableOpacity onPress={() => router.push(`/following`)}>
-                    <Text style={{color: pathName ==="/" ?"black": "red"}}>Following</Text>
-                </TouchableOpacity>
-            </View>
-            <View>
-                <TouchableOpacity onPress={() => router.push(`/@santj5811/post/1`)}>
-                    <Text>게시글 1</Text>
-                </TouchableOpacity>
-            </View>
-            <View>
-                <TouchableOpacity onPress={() => router.push(`/@santj5811/post/2`)}>
-                    <Text>게시글 2</Text>
-                </TouchableOpacity>
-            </View>
-            <View>
-                <TouchableOpacity onPress={() => router.push(`/@santj5811/post/3`)}>
-                    <Text>게시글 3</Text>
-                </TouchableOpacity>
+        <View style={styles.container}>
+            <View style={styles.tabContainer}>
+                <View style={styles.tab}>
+                    <TouchableOpacity onPress={() => router.push(`/`)}>
+                        <Text style={{color: pathName ==="/" ?"red": "black"}}>For you</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.tab}>
+                    <TouchableOpacity onPress={() => router.push(`/following`)}>
+                        <Text style={{color: pathName ==="/" ?"black": "red"}}>Following</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.tab}>
+                    <TouchableOpacity onPress={() => router.push(`/@santj5811/post/1`)}>
+                        <Text>게시글 1</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.tab}>
+                    <TouchableOpacity onPress={() => router.push(`/@santj5811/post/2`)}>
+                        <Text>게시글 2</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.tab}>
+                    <TouchableOpacity onPress={() => router.push(`/@santj5811/post/3`)}>
+                        <Text>게시글 3</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
     );
 }
+
+
+const styles = StyleSheet.create({
+      container: {
+          flex: 1,
+      },
+    tabContainer: {
+          flexDirection: "row",
+    },
+    tab: {
+          flex: 1
+    }
+})
