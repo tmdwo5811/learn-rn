a02ba2425d6c6e209a8a1142d74a0e3fb1eaef48
tmdwo5811 <tmdwo5811@gmail.com>
2026-07-01 14:05:28 +0900

    header blur 적용, blur view 패키지 추가

--- threads/src/app/(tabs)/(home)/index.tsx
+++ threads/src/app/(tabs)/(home)/index.tsx
@@ -1,6 +1,7 @@
-import {Text, TouchableOpacity, View, StyleSheet} from "react-native";
+import {Text, TouchableOpacity, View, StyleSheet, Image, Dimensions} from "react-native";
 import {usePathname, useRouter} from "expo-router";
 import {useSafeAreaInsets} from "react-native-safe-area-context";
+import {BlurView} from "expo-blur";
 // 주소는 변경되지만 화면은 공유해서 사용하는 영역
 export default function Index() {
     const router = useRouter();
@@ -10,35 +11,48 @@ export default function Index() {
     console.log("pathName", pathName);
     console.log("insets", insets);
 
+    const {width, height} = Dimensions.get("window");
+    console.log(`화면 너비: ${width}dp, 화면 높이: ${height}dp`);
+
     return (
         <View style={[styles.container, {paddingTop: insets.top, paddingBottom: insets.bottom}]}>
+            <BlurView style={styles.header} intensity={70}>
+                <Image
+                    source={require("@/assets/images/react-logo.png")}
+                    style={styles.headerLogo}
+                />
+                <TouchableOpacity style={styles.loginButton} onPress={() => router.navigate(`/login`)}>
+                    <Text style={styles.loginButtonText}>로그인</Text>
+                </TouchableOpacity>
+            </BlurView>
             <View style={styles.tabContainer}>
                 <View style={styles.tab}>
-                    <TouchableOpacity onPress={() => router.push(`/`)}>
+                    <TouchableOpacity onPress={() => router.navigate(`/`)}>
                         <Text style={{color: pathName ==="/" ?"red": "black"}}>For you</Text>
                     </TouchableOpacity>
                 </View>
                 <View style={styles.tab}>
-                    <TouchableOpacity onPress={() => router.push(`/following`)}>
+                    <TouchableOpacity onPress={() => router.navigate(`/following`)}>
                         <Text style={{color: pathName ==="/" ?"black": "red"}}>Following</Text>
                     </TouchableOpacity>
                 </View>
-                <View style={styles.tab}>
+            </View>
+                <View>
                     <TouchableOpacity onPress={() => router.push(`/@santj5811/post/1`)}>
                         <Text>게시글 1</Text>
                     </TouchableOpacity>
                 </View>
-                <View style={styles.tab}>
+                <View>
                     <TouchableOpacity onPress={() => router.push(`/@santj5811/post/2`)}>
                         <Text>게시글 2</Text>
                     </TouchableOpacity>
                 </View>
-                <View style={styles.tab}>
+                <View>
                     <TouchableOpacity onPress={() => router.push(`/@santj5811/post/3`)}>
                         <Text>게시글 3</Text>
                     </TouchableOpacity>
                 </View>
-            </View>
+
         </View>
     );
 }
@@ -51,6 +65,28 @@ const styles = StyleSheet.create({
           flexDirection: "row",
     },
     tab: {
-          flex: 1
+      flex: 1,
+      alignItems:"center"
+    },
+    header: {
+      alignItems: "center",
+    },
+    headerLogo: {
+      width: 42,
+      height: 42
+    },
+    loginButton: {
+          position: "absolute",
+        right: 10,
+        top:0,
+        backgroundColor: "black",
+        borderWidth: 1,
+        borderColor: "black",
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 10
+    },
+    loginButtonText: {
+          color: "white"
     }
 })
