3c4f17a69c1e5820d9496b3a9a3aeaa9830c4515
tmdwo5811 <tmdwo5811@gmail.com>
2026-07-03 00:16:36 +0900

    검색 페이지 기본 IU 추가 및 사이드 menu 구성

--- threads/src/app/(tabs)/search.tsx
+++ threads/src/app/(tabs)/search.tsx
@@ -1,13 +1,64 @@
-import { Text, View } from "react-native";
+import {Pressable, StyleSheet, Text, TextInput, View} from "react-native";
+import {useSafeAreaInsets} from "react-native-safe-area-context";
+import {useContext, useState} from "react";
+import {AuthContext} from "@/app/_layout";
+import {Ionicons} from "@expo/vector-icons";
+import SideMenu from "@/components/SideMenu";
 
 export default function Index() {
+    const insets = useSafeAreaInsets();
+    const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+    const [searchQuery, setSearchQuery] = useState("");
+    const {user, login, logout} = useContext(AuthContext);
+    const isLoggedIn = !!user;
     return (
-        <View style={{
-            flex: 1,
-            alignItems: "center",
-            justifyContent: "center",
-        }}>
-            <Text>Edit src/app/search.tsx to edit this screen.</Text>
+        <View style={[styles.container, {paddingTop: insets.top, paddingBottom: insets.bottom}]}>
+            <View style={styles.header}>
+                {isLoggedIn && (
+                    <Pressable style={styles.menuButton} onPress={() => {
+                        setIsSideMenuOpen(true)
+                    }}><Ionicons name="menu" size={24} color="black"/>
+                    </Pressable>
+                )}
+                <SideMenu isVisible={isSideMenuOpen} onClose={() => setIsSideMenuOpen(false)}/>
+            </View>
+            <View style={styles.searchBar}>
+                <TextInput style={styles.searchInput}
+                           placeholder="Search"
+                           value={searchQuery}
+                           onChangeText={setSearchQuery}/>
+            </View>
         </View>
     );
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    header: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        height: 50
+    },
+    menuButton: {
+        position: "absolute",
+        left: 20,
+        top: 10
+    },
+    searchBar: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    searchInput: {
+        width: "90%",
+        height: 40,
+        borderColor: "gray",
+        borderWidth: 1,
+        borderRadius: 20,
+        paddingHorizontal: 10,
+    },
+
+})
